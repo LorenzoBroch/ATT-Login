926c6bef18d38bd6df445e18965cc8fa65b2043e
Lorenzo Broch <160625132+LorenzoBroch@users.noreply.github.com>
2024-04-22 11:39:54 -0300

    Atividade Login

---
+++ ATT_login/Scripts/script.js
@@ -0,0 +1,24 @@
+const usuario = {
+    nome: "lorenzo",
+    senha: "abc"
+}
+
+function login() {
+    var nome = document.getElementById('nome').value
+    var senha = document.getElementById('senha').value
+    
+    if((usuario.nome == nome) && (usuario.senha == senha)) {
+        alert("autenticado")
+        window.location.pathname = "/pagina2.html" //window.location.pathname para mudar a página e redirecionar a partir do nome da pagina'
+    } else {
+        alert("autenticação falhou")
+        document.getElementById('nome').value = ""; //resetando o valor da lable para outra tentativa
+        document.getElementById('senha').value = ""; //resetando o valor da lable para outra tentativa
+        
+    }
+}
+function mostrarResultado() { //pego o elemento do ID de "usuarioNome" na pagina 2 e atribuo ele ao nome e a senha do usuario
+    document.getElementById("usuarioNome").innerHTML = usuario.nome;
+    document.getElementById("usuarioSenha").innerHTML = usuario.senha;
+}
+
